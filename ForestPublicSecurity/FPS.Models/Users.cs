299d7e818f4626d2da3b4b963fb581c232e25186
wangshifei <44600320+wangshifei@users.noreply.github.com>
2018-11-04 11:02:44 +0800

    wsf
    wsf

---
+++ ForestPublicSecurity/FPS.Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPS.Models
+{
+   public class Users
+    {
+        public int ID { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public string RealName { get; set; }
+        public string IdCard { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime AddTime { get; set; }
+        public int State { get; set; }
+        public string Spare { get; set; }
+    }
+}
