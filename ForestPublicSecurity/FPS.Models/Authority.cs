d50b53f02da2a6b7b9fb8cac72c54f223196c4c1
wangshifei <44600320+wangshifei@users.noreply.github.com>
2018-11-04 11:09:13 +0800

    王世飞建model
    王世飞建model

---
+++ ForestPublicSecurity/FPS.Models/Authority.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPS.Models
+{
+   public class Authority
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int FatherId { get; set; }
+        public int State { get; set; }
+        public string Url { get; set; }
+        public int OrderId { get; set; }
+    }
+}
---
+++ ForestPublicSecurity/FPS.Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPS.Models
+{
+   public class Role
+    {
+        public int ID { get; set; }
+        public string RoleName { get; set; }
+        public int State { get; set; }
+    }
+}
