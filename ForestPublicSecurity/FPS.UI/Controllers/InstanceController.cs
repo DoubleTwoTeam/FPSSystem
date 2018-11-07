970e508048302c8a53ae7a4defa37e74f008602f
KonqrHan <42235043+KonqrHan@users.noreply.github.com>
2018-11-07 15:56:50 +0800

    写案件的页面

--- ForestPublicSecurity/FPS.UI/Controllers/InstanceController.cs
+++ ForestPublicSecurity/FPS.UI/Controllers/InstanceController.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 
 using FPS.IServices;
 using FPS.Models;
+using System.IO;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http.Headers;
 
 namespace FPS.UI.Controllers
 {
@@ -15,7 +20,17 @@ namespace FPS.UI.Controllers
         /// 依赖注入案件接口
         /// </summary>
         private readonly IPoliceCase _policeCase;
-        
+
+        private readonly IHostingEnvironment _hostingEnvironment;
+        /// <summary>
+        /// 依赖注入
+        /// </summary>
+        /// <param name="policeCase"></param>
+        public InstanceController(IPoliceCase policeCase,IHostingEnvironment hostingEnvironment)
+        {
+            _policeCase = policeCase;
+            _hostingEnvironment = hostingEnvironment;
+        }
 
         /// <summary>
         /// 案件显示页面
@@ -34,20 +49,33 @@ namespace FPS.UI.Controllers
         {
             return View();
         }
+
         /// <summary>
         /// 案件添加
         /// </summary>
         /// <param name="instance"></param>
         /// <returns></returns>
         [HttpPost]
-        public IActionResult InsertPoliceCase(Instance instance)
+        public IActionResult InsertPoliceCase(Instance instance, IFormFile file)
         {
+            long size = 0;
+            var filename = ContentDispositionHeaderValue
+                            .Parse(file.ContentDisposition)
+                            .FileName
+                            .Trim('"');
+            //_hostingEnvironment.WebRootPath就是要存的地址可以改下
+            filename = _hostingEnvironment.WebRootPath + $@"\{filename}";
+            size += file.Length;
+            using (FileStream fs = System.IO.File.Create(filename))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
 
             return View();
         }
 
-
-        public int  InsertApprove(int id)
+        public int InsertApprove(int id)
         {
             return 0;
         }
