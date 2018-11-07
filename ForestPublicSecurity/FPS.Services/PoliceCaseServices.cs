e8ee335e5017e543f912509d55f5915882e49a68
KonqrHan <42235043+KonqrHan@users.noreply.github.com>
2018-11-07 09:06:56 +0800

    添加案件

--- ForestPublicSecurity/FPS.Services/PoliceCaseServices.cs
+++ ForestPublicSecurity/FPS.Services/PoliceCaseServices.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using FPS.IServices;
+using FPS.Models;
+using SqlSugar;
+
 namespace FPS.Services
 {
-    class PoliceCase
+    public class PoliceCase : IPoliceCase
     {
+
+        /// <summary>
+        /// 添加案件
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
+        public int InsertInstance(Instance instance)
+        {
+            var db = SugerBase.GetInstance();
+
+            int result= db.Insertable(instance).ExecuteCommand();
+
+            return result;
+        }
     }
 }
