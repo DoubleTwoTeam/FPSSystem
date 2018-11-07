3c19b4e0e734a9b85851f65d0df6bec48473c764
KonqrHan <42235043+KonqrHan@users.noreply.github.com>
2018-11-07 13:22:25 +0800

    案件页面、审批接口

--- ForestPublicSecurity/FPS.UI/Controllers/InstanceController.cs
+++ ForestPublicSecurity/FPS.UI/Controllers/InstanceController.cs
@@ -4,13 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using FPS.IServices;
+using FPS.Models;
+
 namespace FPS.UI.Controllers
 {
     public class InstanceController : Controller
     {
-        public IActionResult Index()
+        /// <summary>
+        /// 依赖注入案件接口
+        /// </summary>
+        private readonly IPoliceCase _policeCase;
+        
+
+        public IActionResult GetInstanceList()
         {
             return View();
+        }
+
+        public IActionResult InsertPoliceCase()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult InsertPoliceCase(Instance instance)
+        {
+
+            return View();
+        }
+
+
+        public int  InsertApprove(int id)
+        {
+            return 0;
         }
     }
 }
