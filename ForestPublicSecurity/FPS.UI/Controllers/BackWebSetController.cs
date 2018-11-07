a1562482358b1a88cefa1c455c41e3f90e69ca0d
Zhou <15510482065@163.com>
2018-11-07 20:34:53 +0800

    添加后台首页
    添加后台首页

---
+++ ForestPublicSecurity/FPS.UI/Controllers/BackWebSetController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FPS.UI.Controllers
+{
+    public class BackWebSetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Footer()
+        {
+            return View();
+        }
+
+        public IActionResult Left_menu()
+        {
+            return View();
+        }
+
+        public IActionResult Top()
+        {
+            return View();
+        }
+    }
+}
