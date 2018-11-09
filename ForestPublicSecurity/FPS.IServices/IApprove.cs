3c40d2961697335b420018442838237ab1bd12db
zwjemploy <44597994+zwjemploy@users.noreply.github.com>
2018-11-09 15:00:58 +0800

    Update ForestPublicSecurity/FPS.IServices/IApprove.cs

--- ForestPublicSecurity/FPS.IServices/IApprove.cs
+++ ForestPublicSecurity/FPS.IServices/IApprove.cs
@@ -25,11 +25,11 @@ namespace FPS.IServices
         /// <returns></returns>
         int UpdateApprove(Approve approve);
 
-        /// <summary>
-        /// 审批显示
-        /// </summary>
-        /// <returns></returns>
-        PageList<ApproveDataModel> GetApproveList(PageParams pageParams);
+        ///// <summary>
+        ///// 审批显示
+        ///// </summary>
+        ///// <returns></returns>
+        //PageList<ApproveDataModel> GetApproveList(PageParams pageParams);
 
         /// <summary>
         /// 点击审批页面的某个详情进入查看案情
